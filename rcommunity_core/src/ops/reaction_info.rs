aeaf7ff76d1661a12a35957371fa7e6d5a8accd9
Yu Yin <yxonic@gmail.com>
2022-06-27 02:00:21 +0800

    docs: document ReactionInfo implementors

--- rcommunity_core/src/ops/reaction_info.rs
+++ rcommunity_core/src/ops/reaction_info.rs
@@ -5,6 +5,7 @@ use crate::{error::Result, store::Transaction, utils::typename};
 use crate::markers::Once;
 use crate::markers::{ItemType, ReactionType, UserType};
 
+/// Ability to manage and query reaction basic information.
 #[async_trait]
 pub trait ReactionInfo {
     async fn store_reaction(
@@ -23,6 +24,10 @@ pub trait ReactionInfo {
     ) -> Result<()>;
 }
 
+/// Default [`ReactionInfo`] implementor for all reaction types.
+///
+/// Under the hood, this implementor manages **reaction ID** to **user-item-reaction triplet**
+/// mapping for all reaction types.
 #[async_trait]
 impl<T: ReactionType> ReactionInfo for T {
     default async fn store_reaction(
@@ -51,6 +56,10 @@ impl<T: ReactionType> ReactionInfo for T {
     }
 }
 
+/// Specialized [`ReactionInfo`] implementor for reaction types that are marked as [`Once`].
+///
+/// Under the hood, this implementor manages **reaction ID** to **user-item pair** mapping for
+/// reaction types that react at most once for each user-item pair.
 #[async_trait]
 impl<T: ReactionType + Once> ReactionInfo for T {
     default async fn store_reaction(
